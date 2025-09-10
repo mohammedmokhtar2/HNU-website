5e2f564ed30b93c425b3bb6b66c32be0243ab5dc
mahmoudmatter12 <Mahmoud.Aziz-El-Din.3230080@CIT.hnu.edu.eg>
2025-09-10 10:37:09 +0300

    adjusted cores

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,45 @@
-import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // add cors
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS, PATCH",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+  // Add this section:
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    `${process.env.NEXT_PUBLIC_API_URL}`,
+    "https://ahlan-hnu.vercel.app/"
+  ],
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
       },
     ],
+    domains: ["res.cloudinary.com"],
   },
 };
 
